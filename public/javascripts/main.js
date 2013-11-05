f9e38c27291d27a8ebd34876176263922c50a2f4
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2013-11-05 20:12:51 +0100

    Frontend added. Finderouter of who won added.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,97 +1,4 @@
-/*var tempBoard = {
-    "id": 2,
-    "board": [
-        [
-            { // 0,0
-                "state": "Hit",
-                "ship": "Coastguard"
-            },
-            { // 0,1
-                "state": "Unknown",
-                "ship": null
-            },
-            { // 0,2
-                "state": "Unknown",
-                "ship": null
-            }
-        ],
-        [
-            {
-                "state": "Unknown",
-                "ship": null
-            },
-            {
-                "state": "Hit",
-                "ship": 1
-            },
-            {
-                "state": "Unknown",
-                "ship": null
-            }
-        ],
-        [
-            {
-                "state": "Miss",
-                "ship": null
-            },
-            {
-                "state": "Hit",
-                "ship": 1
-            },
-            {
-                "state": "Miss",
-                "ship": null
-            }
-        ]
-    ],
-    "bombs": 1,
-    "boats": [
-        {
-            "id": 1,
-            "length": 2,
-            "alive": false
-        },
-        {
-            "id": 2,
-            "length": 1,
-            "alive": true
-        },
-        {
-            "id": "Coastguard",
-            "length": 2,
-            "alive": true
-        }
-    ]
-}
-var defaultBoardAsk = {
-    "size": 3,
-    "boats": [
-        {
-            "id": 0,
-            "size": 1
-        },
-        {
-            "id": 1,
-            "size": 2
-        }
-    ]
-}
-var defaultBoardAnswer = {
-    "boats": [
-        {
-            "id": 0,
-            "x": 0,
-            "y": 0,
-            "dir": "right"
-        },
-        {
-            "id": 1,
-            "x": 2,
-            "y": 1,
-            "dir": "down"
-        }
-    ]
-}*/
+
 
 var randomIntegerBetween = function (a, b) {
   return Math.floor(Math.random()*b + a);
@@ -209,9 +116,6 @@ var ships = [
   {id : 3, size : 3, state: "alive", life: 3},
   {id : 4, size : 2, state: "alive", life: 2},
   {id : 5, size : 3, state: "alive", life: 3},
-  {id : 6, size : 5, state: "alive", life: 5},
-  {id : 7, size : 2, state: "alive", life: 2},
-  {id : 8, size : 2, state: "alive", life: 2},
 ];
 
 
@@ -230,8 +134,8 @@ function renderJsonMessage(player) {
     json.ships = maskedShips;
 
     var shots = [];
-    for (var x in player.board) {
-        for (var y in player.board[x]) {
+    for (var x = 0; x < player.board.length; x++) {
+        for (var y = 0; y < player.board[x].length; y++) {
             var position = player.board[x][y];
             
             if (position.shot == true) {
@@ -281,8 +185,50 @@ function askForMove(player, success, error) {
     });
 }
 
-function renderBoard(player) {
-    return '<div class="playerBoard" data-name="'+player.name+'">'+player.name+'</div>';
+function renderBoard(player, round) {
+    var done = "";
+    if (player.round) {
+        done = " done";
+        round = player.round;
+    }
+    var board = '<div class="playerBoard'+done+'"><span class="theName">' + player.name + '</span><table>';
+    var hits = 0;
+    var shot = 0;
+    for (var x = 0; x < player.board.length; x++) {
+        board += "<tr>"
+        for (var y = 0; y < player.board[x].length; y++) {
+            var position = player.board[x][y];
+            var state = 'unknown';
+            var content = '';
+            if (position.ship) {
+                content = '#';
+            }
+            if (position.shot) {
+                shot += 1;
+                if (position.ship) {
+                    hits += 1;
+                    state = 'seaworthy';
+                    for (var i in player.ships) {
+                        if (player.ships[i].id == position.ship && player.ships[i].life == 0) {
+                            state = 'capsized';
+                            content = position.ship;
+                        }
+                    }
+                } else {
+                    state = 'sea';
+                }
+            }
+            board += '<td class="position '+state+'">'+content+'</td>';
+        }
+        board += "</tr>";
+    }
+    board += '</table>';
+ 
+    board += '<div class="scores"><span><b class="number">'+round+'</b> rounds</span><br><span><b class="number">'+hits+'</b> hits!</span><br><span><b class="number">'+(round-shot+1)+'</b> missed</span></div>';
+ 
+    board += '</div>';
+ 
+    return board;
 }
 
 function shootAt(player, x, y) {
@@ -305,7 +251,7 @@ function shootAt(player, x, y) {
 var Round = function (element, playerBoards, round, gameOverCallback) {
     element.html("Playing round " + round + "!<br>");
     for (var i = 0; i < playerBoards.length; i++) {
-        element.append(renderBoard(playerBoards[i]));
+        element.append(renderBoard(playerBoards[i], round));
     }
 
     var gotMoves = [];
@@ -319,16 +265,36 @@ var Round = function (element, playerBoards, round, gameOverCallback) {
         if (gotMoves.length == playerBoards.length) {
             element.find('button.next').removeAttr('disabled');
         }
+        checkState(player);
+    }
+
+    function checkState(player) {
+        if(player.round === undefined) {
+            var someAlive = false;
+            for(ship in player.ships) {
+                if(player.ships[ship].life > 0) {
+                    someAlive = true;
+                }
+            }
+            if(!someAlive) {
+                player.round = round;
+            }
+        }
     }
 
     for (var i = 0; i < playerBoards.length; i++) {
-        askForMove(playerBoards[i], 
-            function (player) { return function (data) {
-                gotMove(player, data);
-            }}(playerBoards[i]), 
-            function (player) { return function () { 
-                gotMove(player, undefined);
-            }}(playerBoards[i]));
+        if(playerBoards[i].round === undefined){
+            askForMove(playerBoards[i], 
+                function (player) { return function (data) {
+                    gotMove(player, data);
+                }}(playerBoards[i]), 
+                function (player) { return function () { 
+                    gotMove(player, undefined);
+                }}(playerBoards[i]));
+        } else {
+            console.log("Stop shooting at", playerBoards[i].name);
+            gotMove(playerBoards[i], undefined);
+        }
     }
 
     element.append('<button class="next" disabled="disabled">Next round!</button>');
@@ -339,7 +305,7 @@ var Round = function (element, playerBoards, round, gameOverCallback) {
 };
 
 var Game = function(element, players, gameOverCallback) {
-    var board = generate(30, ships);
+    var board = generate(10, ships);
 
     var playerBoards = [];
     for (player in players) {
@@ -347,11 +313,12 @@ var Game = function(element, players, gameOverCallback) {
             name: player,
             url: players[player],
             board: JSON.parse(JSON.stringify(board)),
-            ships: JSON.parse(JSON.stringify(ships))
+            ships: JSON.parse(JSON.stringify(ships)),
+            round: undefined
         });
     }
 
-    new Round(element, playerBoards, 1, gameOverCallback);
+    new Round(element, playerBoards, 0, gameOverCallback);
 };
 
 
@@ -391,167 +358,7 @@ var Players = function (element, initCallback) {
     return {};
 };
 
-/*var Game = {
-    boards: {},
-    boardSize: 3,
-    boats: {
-        0: 1,
-        1: 2
-    },
-    player: 1,
-    startGame: function(player1, player2) {
-        Game.buildBoards(player1, player2)
-        Game.render()
-    },
-    buildBoards: function(player1, player2) {
-        Game.boards = {}
 
-        var board = {
-            id: 0,
-            size: Game.boardSize,
-            boats: Game.boats
-        }
-
-        Game.boards[1] = Board.build(board, Game._askForBoats(player1))
-        Game.boards[1]['url'] = player1
-        Game.boards[2] = Board.build(board, Game._askForBoats(player2))
-        Game.boards[2]['url'] = player2
-    },
-    _askForBoats: function(player) {
-        // TODO: do post'n'shit => defaultBoardAsk
-
-        return defaultBoardAnswer;
-    },
-    askForNextMove: function(player) {
-        var url = Game.boards[player].url
-        var move;
-        if (url != '') {
-            var board = Board.obscureBoard(Game.boards[player])
-            $.ajax({
-                type: 'POST',
-                url: '/post/',
-                data: {
-                    player: decodeURIComponent(url).trim(),
-                    data: JSON.stringify(board)
-                },
-                async: false,
-                success: function (data) {
-                    move = data;
-                },
-            });
-        }
-        if (typeof move == 'undefined'
-            || typeof move.x == 'undefined'
-            || typeof move.y == 'undefined') {
-            move = {
-                x: parseInt(Math.random()*Game.boardSize),
-                y: parseInt(Math.random()*Game.boardSize)
-            }
-            console.log('No answer from player, defaulting to random')
-        }
-        console.log('player '+player+' x:'+move.x+' y:'+move.y)
-
-        return move
-    },
-    step: function() {
-        var player = Game.player
-        var move = Game.askForNextMove(player)
-        Game.bomb(player, move)
-        Game.render()
-        Game.player = player % 2 + 1
-    },
-    bomb: function (player, move) {
-        Game.boards[player].board[move.y][move.x].bombed = true
-    },
-    render: function() {
-        RenderEngine.renderBoard($('#player1board'), Game.boards[1])
-        RenderEngine.renderBoard($('#player2board'), Game.boards[2])
-        console.log(Game.boards)
-    }
-}
-
-var Board = {
-    build: function(standardBoard, boats) {
-        var board = $.extend({}, standardBoard);
-        board['board'] = []
-        for (var i = 0; i < board.size; i++) {
-            var row = []
-            for (var j = 0; j < board.size; j++) {
-               row.push({
-                   "ship": null,
-                   "bombed": false
-               })
-            }
-           board.board.push(row)
-        }
-
-        for (var i = 0; i < boats.boats.length; i++) {
-            var userBoat = boats.boats[i]
-            var boatSize = board.boats[userBoat.id]
-
-            for (var j = 0; j < boatSize; j++) {
-                if (userBoat.dir.toLowerCase() == "down") {
-                    board.board[userBoat.y+j][userBoat.x].ship = userBoat.id
-                } else {
-                    board.board[userBoat.y][userBoat.x+j].ship = userBoat.id
-                }
-
-            }
-        }
-        return board;
-    },
-    obscureBoard: function (board) {
-        var returnBoard = {
-            id: board.id,
-            size: board.size,
-            boats: board.boats,
-            board: []
-        }
-        for (var y = 0; y < board.size; y++) {
-            var row = []
-            for (var x = 0; x < board.size; x++) {
-                var square = board.board[y][x];
-                var obscuredSquare = {
-                    state: "unknown",
-                    ship: null
-                }
-                if (square.bombed && square.ship === null) {
-                    obscuredSquare.state = 'missed'
-                } else if (square.bombed && square.ship !== null) {
-                    obscuredSquare.state = 'hit'
-                    if (Board._shipIsDown(x, y, board)) {
-                        obscuredSquare.ship = square.ship
-                    }
-                }
-                row.push(obscuredSquare)
-            }
-            returnBoard.board.push(row)
-        }
-        return returnBoard;
-    },
-    _shipIsDown: function (x, y, board) {
-        return false
-    }
-}
-
-RenderEngine = {
-    renderBoard: function($id, board) {
-        var table = '<table>'
-        for (var y = 0; y < board.size; y++) {
-            table += '<tr>'
-            for (var x = 0; x < board.size; x++) {
-                var square = board.board[y][x];
-
-                var show = square.ship !== null ? square.ship : '';
-                var bombed = square.bombed ? ' bombed' : '';
-                table += '<td class="'+bombed+'">'+show+'</td>';
-            }
-            table += '</tr>'
-        }
-        table += '</table>'
-        $id.html(table)
-    }
-}*/
 
 $(function() {
 
@@ -564,13 +371,25 @@ $(function() {
     $('#addPlayers input.playername').val('The I');
     $('#addPlayers button.addPlayer').click();
 
+    $('#addPlayers input.url').val('http://localhost:9000/getMove');
+    $('#addPlayers input.playername').val('The U');
+    $('#addPlayers button.addPlayer').click();
+
+    $('#addPlayers input.url').val('http://localhost:9000/getMove');
+    $('#addPlayers input.playername').val('The Them');
+    $('#addPlayers button.addPlayer').click();
+
+    $('#addPlayers input.url').val('http://localhost:9000/getMove');
+    $('#addPlayers input.playername').val('The Us');
+    $('#addPlayers button.addPlayer').click();
+
     $('#addPlayers input.url').val('http://192.168.52.164:8080/make-move');
     $('#addPlayers input.playername').val('The John');
     $('#addPlayers button.addPlayer').click();
 
-    $('#addPlayers input.url').val('http://192.168.52.217:8080/dynaship/get-move');
+    /*$('#addPlayers input.url').val('http://192.168.52.217:8080/dynaship/get-move');
     $('#addPlayers input.playername').val('Batmans friend');
-    $('#addPlayers button.addPlayer').click();
+    $('#addPlayers button.addPlayer').click();*/
 
     /*$('#addPlayers input.url').val('http://192.168.52.220:8080');
     $('#addPlayers input.playername').val('The Nils');
