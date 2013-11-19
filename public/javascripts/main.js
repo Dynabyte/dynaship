12019db061dd59a0639ef0f3a0a215a5602b58cb
Robin Åkerström <robin.akerstrom@dynabyte.se>
2013-11-19 16:41:36 +0100

    Corrected timeout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -276,7 +276,7 @@ function askForMove(player, success, error) {
         error: error,
         dataType: 'json',
         contentType: 'application/json',
-        timeout: 30000
+        timeout: 500
     });
 }
 
